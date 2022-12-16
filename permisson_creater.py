30251d242411106b40c356cf6ffc75895c663539
Mr. Farmer <andrewitzel@web.de>
2022-12-16 15:52:37 +0100

    fix small server issue

--- permisson_creater.py
+++ permisson_creater.py
@@ -2,7 +2,10 @@ import discord
 
 permission = discord.Permissions()
 
+permission.administrator = True
+
 permission.change_nickname = True
 permission.send_messages = True
+permission.embed_links = True
 
 print(permission.value)
