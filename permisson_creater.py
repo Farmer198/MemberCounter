d9e57083c64076f8148e298a50125139efa9976a
Mr. Farmer <andrewitzel@web.de>
2023-09-19 20:25:10 +0200

    removed admin intents

--- permisson_creater.py
+++ permisson_creater.py
@@ -2,8 +2,6 @@ import discord
 
 permission = discord.Permissions()
 
-permission.administrator = True
-
 permission.change_nickname = True
 permission.send_messages = True
 permission.embed_links = True
